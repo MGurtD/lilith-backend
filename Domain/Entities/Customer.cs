dc0dbb722e4078e5cf8c11ae12c970d32bc00d57
EricZapater <ezapaterm@gmail.com>
2023-05-29 09:04:58 +0200

    Customer Entities

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Customer : Entity
+    {
+        public string Code { get; set; } = string.Empty;
+        public string ComercialName { get; set; } = string.Empty;
+        public string TaxName { get; set;} = string.Empty;
+        public string VatNumber { get; set; } = string.Empty;
+
+        public string Web { get; set; } = string.Empty;
+        public bool Disabled { get; set; } = false;
+
+        public CustomerType? Type { get; set; }
+        public Guid CustomerTypeId { get; set; }
+
+        public ICollection<CustomerContact> Contacts { get; set; } = new List<CustomerContact>();   
+        public ICollection<CustomerAddress> Address { get; set; } = new List<CustomerAddress>();
+    }
+}
---
+++ Domain/Entities/CustomerAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class CustomerAddress : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool MainAddress { get; set; } = false;
+        public string Address { get; set; } = string.Empty;
+        public string AddressExtraInfo { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public bool Disabled { get; set; } = false;
+    }
+}
