0ded2cad16be66dbf7c6b0eded6bb5a3115b9ded
Marc Gurt <marc.gurt@mapex.io>
2023-12-12 17:21:36 +0100

    refactor: workmaster phase entites

--- Domain/Entities/Production/WorkMasterPhaseBillOfMaterials.cs
+++ Domain/Entities/Production/WorkMasterPhaseBillOfMaterials.cs
@@ -1,4 +1,3 @@
-using Domain.Entities;
 using Domain.Entities.Shared;
 
 namespace Domain.Entities.Production;
@@ -6,10 +5,12 @@ public class WorkMasterPhaseBillOfMaterials: Entity
 {
     public Guid WorkMasterPhaseId {get; set;}
     public WorkMasterPhase? WorkMasterPhase {get; set;}
-    public Guid ReferenceId {get; set;}
-    public Reference? Reference {get; set;}
-    public decimal Quantity {get; set;}
-    public Guid? WasteReferenceId {get;set;}
-    public Reference? WasteReference {get; set;}
-    public decimal Waste {get;set;}
+    public Guid ReferenceId { get; set; }
+    public Reference? Reference { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal Width { get; set; }
+    public decimal Length { get; set; }
+    public decimal Height { get; set; }
+    public decimal Diameter { get; set; }
+    public decimal Thickness { get; set; }
 }
