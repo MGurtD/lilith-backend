3edda9d7de184042e6ab764f4732b0ddca0026ed
Marc Gurt <marcgurt.developer@gmail.com>
2023-12-18 16:06:01 +0100

    Reference as a Service (#12)
    * Move density to reference type
    
    * Remove comments
    
    * refactor: workmaster phase entites
    
    * refactor: move file to other directory
    
    * add isservice
    
    * Change response
    
    ---------
    
    Co-authored-by: EricZapater <ezapaterm@gmail.com>
    Co-authored-by: Marc Gurt <marc.gurt@mapex.io>

--- Domain/Entities/Production/WorkMasterPhaseDetail.cs
+++ Domain/Entities/Production/WorkMasterPhaseDetail.cs
@@ -1,22 +1,17 @@
-using Domain.Entities;
-using Domain.Entities.Production;
-
 namespace Domain.Entities.Production;
+
 public class WorkMasterPhaseDetail : Entity
 {
-    public Guid WorkMasterPhaseId {get; set;}
-    public WorkMasterPhase? WorkMasterPhase { get; set;}
-    public Guid WorkcenterTypeId {get; set;}
-    public WorkcenterType? WorkcenterType {get; set;}
-    public Guid? PreferredWorkcenterId {get; set;}
-    public Workcenter? PreferredWorkcenter {get; set;}
-    public Guid OperatorTypeId {get; set;}
-    public OperatorType? OperatorType {get; set;}
-    public Guid MachineStatusId {get; set;}
-    public MachineStatus? MachineStatus{get; set;}
-    public decimal EstimatedTime {get; set;}
-    public bool IsCycleTime {get;set;}
-    public bool IsExternalWork {get; set;}
-    public decimal ExternalWorkCost {get; set; }
-    
+    public Guid WorkMasterPhaseId { get; set;}
+    public WorkMasterPhase? WorkMasterPhase { get; set; }    
+    public Guid? MachineStatusId { get; set;}
+    public MachineStatus? MachineStatus{ get; set; }
+    public decimal EstimatedTime { get; set; }
+    public bool IsCycleTime { get;set; }
+    public bool IsExternalWork { get; set; }
+    public decimal ExternalWorkCost { get; set; }
+
+    public int Order { get; set; }
+    public string Comment { get; set; } = string.Empty;
+
 }
