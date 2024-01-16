1c8fa218ff8193fd5aa08fe8db1c80fcaf691127
Marc Gurt <marc.gurt@mapex.io>
2024-01-16 20:06:05 +0100

    changed: workmaster plant model relations nullable

--- Domain/Entities/Production/WorkMasterPhase.cs
+++ Domain/Entities/Production/WorkMasterPhase.cs
@@ -7,9 +7,9 @@ public class WorkMasterPhase : Entity
     public Guid WorkMasterId {get; set;}
     public WorkMaster? WorkMaster {get; set;}
 
-    public Guid OperatorTypeId { get; set; }
+    public Guid? OperatorTypeId { get; set; }
     public OperatorType? OperatorType { get; set; }
-    public Guid WorkcenterTypeId { get; set; }
+    public Guid? WorkcenterTypeId { get; set; }
     public WorkcenterType? WorkcenterType { get; set; }
     public Guid? PreferredWorkcenterId { get; set; }
     public Workcenter? PreferredWorkcenter { get; set; }
