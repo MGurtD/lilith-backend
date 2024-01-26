a043803543dcdb99460d37b9c910c5e458e14f32
Marc Gurt <marc.gurt@mapex.io>
2024-01-26 19:46:14 +0100

    removed: unnecessary data on work order creation

--- Application/Contracts/Production/CreateWorkOrderDto.cs
+++ Application/Contracts/Production/CreateWorkOrderDto.cs
@@ -5,8 +5,6 @@
         public Guid WorkMasterId { get; set; }
         public decimal PlannedQuantity { get; set; }
         public DateTime PlannedDate { get; set; }
-        public Guid? SalesOrderId { get; set; }
-        public Guid? SalesOrderDetailId { get; set; }
         public string Comment { get; set; } = string.Empty;
-}
+    }
 }
