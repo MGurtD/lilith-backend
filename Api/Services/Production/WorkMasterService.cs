79bfcb7e7a30850b0939df4f1b323fc94c7c0c55
EricZapater <ezapaterm@gmail.com>
2024-01-29 08:39:43 +0100

    First branch commit

---
+++ Api/Services/Production/WorkMasterService.cs
@@ -0,0 +1,36 @@
+﻿using Application.Contracts;
+using Application.Persistance;
+using Application.Services.Production;
+using Domain.Entities.Production;
+
+namespace Api.Services.Production
+{
+    public class WorkMasterService : IWorkMasterService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public WorkMasterService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<GenericResponse> Calculate(WorkMaster workMaster)
+        {
+            var result decimal = 0.0M;
+            //Recollir la quantitat base
+            var baseQuantity = workMaster.BaseQuantity;
+            //Recorrer les phases
+            //A cada fase, recollir el operatortypeId, i buscar el seu preu cost/hora
+            //Per cada fase recorrer els detalls i obtenim el temps+estat maquina, es busca el cost del binomi
+            //Si es temps de cicle es multiplica per la quantitat base, sinó es el temps del bloc. Es multiplica el temps pel cost
+            //temps en minuts cost en hores
+            foreach(var phase in workMaster.Phases)
+            {
+                phase.
+            }
+            //
+
+            return new GenericResponse(false, result);
+        }
+    }
+}
---
+++ Application/Services/Production/IWorkMasterService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Contracts.Purchase;
+using Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Production;
+
+namespace Application.Services.Production
+{
+    public interface IWorkMasterService
+    {
+        Task<GenericResponse> Calculate(WorkMaster workMaster);
+    }
+}
