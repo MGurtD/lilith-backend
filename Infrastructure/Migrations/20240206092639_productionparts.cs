2ed10783b50b968b89d40e472488ab5c87fbc666
EricZapater <ezapaterm@gmail.com>
2024-02-06 10:44:02 +0100

    Production Parts entity and migrations

---
+++ Infrastructure/Migrations/20240206092639_productionparts.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class productionparts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductionParts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    WorkcenterId = table.Column<Guid>(type: "uuid", nullable: false),
+                    WorkorderphasedetailId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OperatorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Time = table.Column<decimal>(type: "numeric(18,4)", precision: 18, scale: 4, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    Disabled = table.Column<bool>(type: "bool", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductionParts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductionParts_Operators_OperatorId",
+                        column: x => x.OperatorId,
+                        principalTable: "Operators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductionParts_Workcenters_WorkcenterId",
+                        column: x => x.WorkcenterId,
+                        principalTable: "Workcenters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductionParts_WorkOrderPhaseDetail_WorkorderphasedetailId",
+                        column: x => x.WorkorderphasedetailId,
+                        principalTable: "WorkOrderPhaseDetail",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_productionpart_date",
+                table: "ProductionParts",
+                column: "Date");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_workcenter_phasedetail_operator",
+                table: "ProductionParts",
+                columns: new[] { "WorkcenterId", "OperatorId", "WorkorderphasedetailId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductionParts_OperatorId",
+                table: "ProductionParts",
+                column: "OperatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductionParts_WorkorderphasedetailId",
+                table: "ProductionParts",
+                column: "WorkorderphasedetailId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductionParts");
+        }
+    }
+}
---
+++ Infrastructure/Persistance/EntityConfiguration/Production/ProductionPartsBuilder.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities.Production;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistance.EntityConfiguration.Production
+{
+    public class ProductionPartsBuilder : IEntityTypeConfiguration<ProductionParts>
+    {
+        public void Configure(EntityTypeBuilder<ProductionParts> builder)
+        {
+            builder.ConfigureBase();
+            builder
+                .Property(b => b.Date)
+                .IsRequired()
+                .HasColumnType("timestamp without time zone");
+            builder
+                .Property(b => b.Quantity)
+                .IsRequired()
+                .HasColumnType("int");
+            builder
+                .Property(b => b.Time)
+                .IsRequired()
+                .HasColumnType("decimal")
+                .HasPrecision(ApplicationDbContextConstants.DECIMAL_PRECISION,
+                             ApplicationDbContextConstants.DECIMAL_SCALE); ;
+            builder
+                .HasKey(b => b.Id)
+                .HasName("PK_ProductionParts");
+            builder
+                .HasIndex(b => new { b.WorkcenterId, b.OperatorId, b.WorkorderphasedetailId })
+                .HasDatabaseName("idx_workcenter_phasedetail_operator");
+            builder
+                .HasIndex(b => b.Date)
+                .HasDatabaseName("idx_productionpart_date");
+
+            builder.ToTable("ProductionParts");
+                
+        }
+    }
+}
