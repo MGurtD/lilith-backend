02beaccb52287a1fd194285697ee434d4e5c0547
Marc Gurt <marc.gurt@mapex.io>
2024-02-13 18:45:16 +0100

    removed: unnecessary lines

--- Domain/Repositories/Purchase/IPurchaseInvoiceRepository.cs
+++ Domain/Repositories/Purchase/IPurchaseInvoiceRepository.cs
@@ -4,14 +4,12 @@ namespace Application.Persistance.Repositories.Purchase
 {
     public interface IPurchaseInvoiceRepository : IRepository<PurchaseInvoice, Guid>
     {
-
         IRepository<PurchaseInvoiceImport, Guid> ImportsRepository { get; }
 
         Task AddImport(PurchaseInvoiceImport import);
         Task UpdateImport(PurchaseInvoiceImport import);
         Task<bool> RemoveImport(Guid id);
 
-        //int GetNextNumber();
 
     }
 }
