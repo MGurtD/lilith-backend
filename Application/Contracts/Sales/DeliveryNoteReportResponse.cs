487a926ee010e5c0389bc8439ea4086b3c13b537
Marc Gurt <marc.gurt@mapex.io>
2024-04-16 08:05:47 +0200

    deleted: dto warnings

--- Application/Contracts/Sales/DeliveryNoteReportResponse.cs
+++ Application/Contracts/Sales/DeliveryNoteReportResponse.cs
@@ -4,10 +4,10 @@ namespace Domain.Entities.Sales
 {
     public class DeliveryNoteReportResponse
     {
-        public Customer Customer { get; set; }
-        public Site Site { get; set; }
-        public DeliveryNote DeliveryNote { get; set; }
-        public IList<SalesOrderHeader> Orders { get; set; }
+        public Customer? Customer { get; set; }
+        public Site? Site { get; set; }
+        public DeliveryNote? DeliveryNote { get; set; }
+        public IList<SalesOrderHeader>? Orders { get; set; }
         public decimal Total { get; set; }
     }
 }
