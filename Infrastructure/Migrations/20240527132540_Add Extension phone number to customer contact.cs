f96ae10fce08910d02b58793a958f51dc39a02ea
EricZapater <ezapaterm@gmail.com>
2024-05-27 15:35:42 +0200

    Add extension to customer contact

---
+++ Infrastructure/Migrations/20240527132540_Add Extension phone number to customer contact.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddExtensionphonenumbertocustomercontact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Extension",
+                table: "CustomerContacts",
+                type: "varchar",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Extension",
+                table: "CustomerContacts");
+        }
+    }
+}
