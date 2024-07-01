d2da1a1bca0e449be092b8344936b263790c6869
Marc Gurt <marc.gurt@mapex.io>
2024-07-01 08:10:11 +0200

    added: planning controller to get the information from the workcenter detail screen

--- Domain/Entities/Production/Workcenter.cs
+++ Domain/Entities/Production/Workcenter.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Domain.Entities.Production;
 
-namespace Domain.Entities.Production
+public class Workcenter : Entity
 {
-    public class Workcenter : Entity
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public double costHour { get; set; } = 0.0;
-        public Guid AreaId { get; set; }
-        public Area? Area { get; set; }
-        public Guid WorkcenterTypeId { get; set; }
-        public WorkcenterType? WorkcenterType { get; set; }
-        public Guid ShiftId { get; set; }
-        public Shift? Shift { get; set; }
-    }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public double costHour { get; set; } = 0.0;
+    public Guid AreaId { get; set; }
+    public Area? Area { get; set; }
+    public Guid WorkcenterTypeId { get; set; }
+    public WorkcenterType? WorkcenterType { get; set; }
+    public Guid ShiftId { get; set; }
+    public Shift? Shift { get; set; }
 }
