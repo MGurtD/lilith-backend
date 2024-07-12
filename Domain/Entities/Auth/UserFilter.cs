054223905bf5ce840d6b7c904d5a86c8e9bdaaa3
Marc Gurt <marc.gurt@mapex.io>
2024-07-12 16:11:12 +0200

    added: user filter entity

---
+++ Domain/Entities/Auth/UserFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities.Auth
+{
+    public class UserFilter : Entity
+    {
+        public string Page { get; set; } = string.Empty;
+        public string Key { get; set; } = string.Empty;
+        public string Filter { get; set; } = string.Empty;
+
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+
+    }
+}
