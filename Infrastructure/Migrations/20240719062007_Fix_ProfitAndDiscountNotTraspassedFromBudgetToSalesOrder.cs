69c5f913d34468ccb512a1b93d4faaff2173de92
Marc Gurt <marcgurt.developer@gmail.com>
2024-07-19 08:23:39 +0200

    Sc 199/guardar filtres a les diferents pagines (#42)
    * added: user filter entity
    
    * added: user filter repository
    added: user filter controller
    
    * added: get method
    
    * added: endpoint to get user all user filters
    
    * fixed: discount & profit details on create order from budget
    
    * fixed: discount and profit updating from budget to sales order
    
    ---------
    
    Co-authored-by: Marc Gurt <marc.gurt@mapex.io>

---
+++ Infrastructure/Migrations/20240719062007_Fix_ProfitAndDiscountNotTraspassedFromBudgetToSalesOrder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Fix_ProfitAndDiscountNotTraspassedFromBudgetToSalesOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"SalesOrderDetail\" OD\r\nSET \r\n    \"Profit\" = BD.\"Profit\",\r\n    \"Discount\" = BD.\"Discount\"\r\nFROM \r\n    \"BudgetDetails\" BD \r\n    JOIN \"Budgets\" B ON BD.\"BudgetId\" = B.\"Id\"\r\n    JOIN \"SalesOrderHeader\" O ON O.\"BudgetId\" = B.\"Id\"\r\nWHERE\r\n    OD.\"SalesOrderHeaderId\" = O.\"Id\" AND\r\n    BD.\"ReferenceId\" = OD.\"ReferenceId\" AND\r\n    (BD.\"Profit\" <> OD.\"Profit\" OR BD.\"Discount\" <> OD.\"Discount\");");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
