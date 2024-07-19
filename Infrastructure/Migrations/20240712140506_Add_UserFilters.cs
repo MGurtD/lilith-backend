6f5d84be68f7534043458fcd1632368a3bd66957
Marc Gurt <marcgurt.developer@gmail.com>
2024-07-19 08:09:00 +0200

    feat: [sc-199] Guardar filtres a les diferents pàgines (#41)
    * added: user filter entity
    
    * added: user filter repository
    added: user filter controller
    
    * added: get method
    
    * added: endpoint to get user all user filters
    
    * fixed: discount & profit details on create order from budget
    
    ---------
    
    Co-authored-by: Marc Gurt <marc.gurt@mapex.io>

---
+++ Infrastructure/Migrations/20240712140506_Add_UserFilters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Add_UserFilters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFilters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Page = table.Column<string>(type: "varchar", maxLength: 250, nullable: false),
+                    Key = table.Column<string>(type: "varchar", maxLength: 500, nullable: false),
+                    Filter = table.Column<string>(type: "varchar", maxLength: 4000, nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    Disabled = table.Column<bool>(type: "bool", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFilter", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserFilters_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFilters_UserId",
+                table: "UserFilters",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFilters");
+        }
+    }
+}
---
+++ Infrastructure/Persistance/EntityConfiguration/Auth/UserFilterBuilder.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities.Auth;
+
+namespace Infrastructure.Persistance.EntityConfiguration.Auth
+{
+    public class UserFilterBuilder : IEntityTypeConfiguration<UserFilter>
+    {
+        public void Configure(EntityTypeBuilder<UserFilter> builder)
+        {
+            builder.ConfigureBase();
+
+            builder
+                .Property(b => b.Page)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(250);
+            builder
+                .Property(b => b.Key)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(500);
+            builder
+                .Property(b => b.Filter)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(4000);
+            builder
+                .Property(b => b.Disabled)
+                .IsRequired()
+                .HasColumnType("bool")
+                .HasDefaultValue(false);
+
+            builder
+                .HasKey(b => b.Id)
+                .HasName($"PK_UserFilter");
+
+            builder.ToTable("UserFilters");
+        }
+
+    }
+}
