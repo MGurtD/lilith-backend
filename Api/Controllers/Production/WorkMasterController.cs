160e810827c5fc29ffb6f133267b6fd4836e3d43
Marc Gurt <marc.gurt@mapex.io>
2024-08-28 19:20:37 +0200

    added: stored procedure on migration refactor: improve message on workmaster copy fail

--- Api/Controllers/Production/WorkMasterController.cs
+++ Api/Controllers/Production/WorkMasterController.cs
@@ -110,7 +110,7 @@ namespace Api.Controllers.Production
             if (request.ReferenceId.HasValue && request.ReferenceId != Guid.Empty)
             {
                 var exists = _unitOfWork.WorkMasters.Find(w => w.ReferenceId == request.ReferenceId && w.Mode == request.Mode).Any();
-                if (exists) return Conflict(new GenericResponse(false, $"Referencia de destí amb ruta de fabricació existent"));
+                if (exists) return Conflict(new GenericResponse(false, $"Referencia amb ruta de fabricació del mode seleccionat. Seleccioni un altre mode"));
             }
             
 
