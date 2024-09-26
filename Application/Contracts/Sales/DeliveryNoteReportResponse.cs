3e91438f824bdbbe422ef8a5a92144b556977494
Marc Gurt <marc.gurt@mapex.io>
2024-09-26 20:02:23 +0200

    added: delivery note order's total

--- Application/Contracts/Sales/DeliveryNoteReportResponse.cs
+++ Application/Contracts/Sales/DeliveryNoteReportResponse.cs
@@ -7,7 +7,16 @@ namespace Domain.Entities.Sales
         public Customer? Customer { get; set; }
         public Site? Site { get; set; }
         public DeliveryNote? DeliveryNote { get; set; }
-        public IList<SalesOrderHeader>? Orders { get; set; }
+        public IList<DeliveryNoteOrderReportDto>? Orders { get; set; }
         public decimal Total { get; set; }
+    }
+
+    public class DeliveryNoteOrderReportDto
+    {
+        public required string Number { get; set; }
+        public required DateTime Date { get; set; }
+        public required string CustomerNumber { get; set; }
+        public required decimal Total { get; set; }
+        public required List<SalesOrderDetail> Details { get; set; }
     }
 }
