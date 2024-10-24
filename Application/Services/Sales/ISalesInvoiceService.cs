43f7405ecef4164e01414be2b6cd8082e956d4f4
Marc Gurt <marcgurt.developer@gmail.com>
2024-10-24 19:48:07 +0200

    added: sales invoice management by external manager (#62)
    Co-authored-by: Marc Gurt <marc.gurt@mapex.io>

--- Application/Services/Sales/ISalesInvoiceService.cs
+++ Application/Services/Sales/ISalesInvoiceService.cs
@@ -1,4 +1,6 @@
+using Application.Contract;
 using Application.Contracts;
+using Application.Contracts.Purchase;
 using Application.Contracts.Sales;
 using Domain.Entities.Sales;
 
@@ -14,12 +16,14 @@ public interface ISalesInvoiceService
     Task<InvoiceReportDto?> GetDtoForReportingById(Guid id);
     IEnumerable<SalesInvoice> GetBetweenDates(DateTime startDate, DateTime endDate);
     IEnumerable<SalesInvoice> GetBetweenDatesAndStatus(DateTime startDate, DateTime endDate, Guid statusId);
+    IEnumerable<SalesInvoice> GetBetweenDatesAndExcludeStatus(DateTime startDate, DateTime endDate, Guid excludeStatusId);
     IEnumerable<SalesInvoice> GetBetweenDatesAndCustomer(DateTime startDate, DateTime endDate, Guid customerId);
     IEnumerable<SalesInvoice> GetByCustomer(Guid customerId);
     IEnumerable<SalesInvoice> GetByStatus(Guid statusId);
     IEnumerable<SalesInvoice> GetByExercise(Guid exerciseId);
 
     Task<GenericResponse> Update(SalesInvoice SalesInvoice);
+    Task<GenericResponse> ChangeStatuses(ChangeStatusOfInvoicesRequest changeStatusOfPurchaseInvoicesRequest);
     Task<GenericResponse> Remove(Guid id);
 
     Task<GenericResponse> AddDeliveryNote(Guid id, DeliveryNote deliveryNote);
