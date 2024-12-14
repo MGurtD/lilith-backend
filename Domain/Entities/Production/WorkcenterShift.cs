f7db52b9fe962928efff8fbc0f984891357a2f69
Marc Gurt <marc.gurt@mapex.io>
2024-12-14 11:48:47 +0100

    added: entities

---
+++ Domain/Entities/Production/WorkcenterShift.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Production;
+
+public class WorkcenterShift : Entity
+{
+    public Guid WorkcenterId { get; set; }
+    public Workcenter? Workcenter { get; set; }
+    public Guid ShiftDetailId { get; set; }
+    public ShiftDetail? ShiftDetail { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    public ICollection<WorkcenterShiftDetail> Details { get; set; } = [];
+
+    [NotMapped]
+    public override DateTime CreatedOn { get => base.CreatedOn; set => base.CreatedOn = value; }
+    [NotMapped]
+    public override DateTime UpdatedOn { get => base.UpdatedOn; set => base.UpdatedOn = value; }
+}
---
+++ Domain/Entities/Production/WorkcenterShiftDetail.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Production;
+
+public class WorkcenterShiftDetail : Entity
+{
+    public Guid WorkcenterShiftId { get; set; }
+    public WorkcenterShift? WorkcenterShift { get; set; }
+    public Guid MachineStatusId { get; set; }
+    public MachineStatus? MachineStatus { get; set; }
+    public Guid? OperatorId { get; set; }
+    public Operator? Operator { get; set; }
+    public Guid? WorkOrderPhaseId { get; set; }
+    public WorkOrderPhase? WorkOrderPhase { get; set; }
+    public decimal QuantityOk { get; set; }
+    public decimal QuantityKo { get; set; }
+
+    public DateTime StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+
+    [NotMapped]
+    public override DateTime CreatedOn { get => base.CreatedOn; set => base.CreatedOn = value; }
+    [NotMapped]
+    public override DateTime UpdatedOn { get => base.UpdatedOn; set => base.UpdatedOn = value; }
+}
