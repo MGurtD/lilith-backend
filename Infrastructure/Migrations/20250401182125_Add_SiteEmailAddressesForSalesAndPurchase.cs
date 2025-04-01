9add40490c94c57ff48295d03b4492aed70363a5
Marc Gurt <marc.gurt@mapex.io>
2025-04-01 20:25:55 +0200

    added: EmailSales & EmailPurchase fields on Site entity

---
+++ Infrastructure/Migrations/20250401182125_Add_SiteEmailAddressesForSalesAndPurchase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Add_SiteEmailAddressesForSalesAndPurchase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailPurchase",
+                table: "Sites",
+                type: "varchar",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailSales",
+                table: "Sites",
+                type: "varchar",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailPurchase",
+                table: "Sites");
+
+            migrationBuilder.DropColumn(
+                name: "EmailSales",
+                table: "Sites");
+        }
+    }
+}
