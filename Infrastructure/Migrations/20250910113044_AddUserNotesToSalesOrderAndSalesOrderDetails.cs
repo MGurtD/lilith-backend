aa402ab8ea17cd6a357a316c694d21f9a928c9f3
EricZapater <ezapaterm@gmail.com>
2025-09-10 13:41:39 +0200

    Feat: Add user notes to sales order and sales order details

---
+++ Infrastructure/Migrations/20250910113044_AddUserNotesToSalesOrderAndSalesOrderDetails.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNotesToSalesOrderAndSalesOrderDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserNotes",
+                table: "SalesOrderHeader",
+                type: "varchar",
+                maxLength: 4000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserNotes",
+                table: "SalesOrderDetail",
+                type: "varchar",
+                maxLength: 4000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserNotes",
+                table: "SalesOrderHeader");
+
+            migrationBuilder.DropColumn(
+                name: "UserNotes",
+                table: "SalesOrderDetail");
+        }
+    }
+}
