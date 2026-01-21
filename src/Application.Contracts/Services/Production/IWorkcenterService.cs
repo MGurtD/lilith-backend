7f0290d10da05c5003e2bde4900725c6accad675
EricZapater <ezapaterm@gmail.com>
2026-01-21 12:04:47 +0100

    Fix errors

--- src/Application.Contracts/Services/Production/IWorkcenterService.cs
+++ src/Application.Contracts/Services/Production/IWorkcenterService.cs
@@ -8,6 +8,7 @@ public interface IWorkcenterService
     Task<Workcenter?> GetById(Guid id);
     Task<IEnumerable<Workcenter>> GetAll();
     Task<IEnumerable<Workcenter>> GetVisibleInPlant();
+    Task<IEnumerable<WorkcenterLoadDto>> GetWorkcenterLoadBetweenDatesByWorkcenterType(DateTime startDate, DateTime endDate);
     Task<GenericResponse> Create(Workcenter workcenter);
     Task<GenericResponse> Update(Workcenter workcenter);
     Task<GenericResponse> Remove(Guid id);
